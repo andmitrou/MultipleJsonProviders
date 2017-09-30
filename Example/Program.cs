fd8f94c1a1af6d49b7203b7412551edf68df1665
andmitrou <donmondi@gmail.com>
2017-09-30 12:17:46 +0300

    Main method modified

--- Example/Program.cs
+++ Example/Program.cs
@@ -10,16 +10,23 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            //Create a dictionary object to be converted
+            //Example Create a dictionary object to be converted
             var dict = new Dictionary<string, object>();
             dict.Add("Name", "Contoso");
             dict.Add("Surname", "Ostoso");
             dict.Add("Age", "27");
 
-            //Create Providers and select provider to Serialise
+            //Create Providers and select Native provider to Serialise
             var parsers = new JsonProviders();
-            IJsonProvider parser = parsers.GetProvider(ProviderEnum.NewtonsoftProvider);
-            var json = parser.Serialise<Dictionary<string, object>>(dict);
+            IJsonProvider nativeParser = parsers.GetProvider(ProviderEnum.NativeProvider);
+            var json = nativeParser.Serialise<Dictionary<string, object>>(dict);
+
+            //Now use different provider to deserialize it to object 
+            IJsonProvider newtonParser = parsers.GetProvider(ProviderEnum.NewtonsoftProvider);
+            var resultObject = newtonParser.Deserialize<Dictionary<String, object>>(json);
+
         }
+
+      
     }
 }
